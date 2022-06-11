3d131ce187187641a02d55aad8ef57d4d99eb29b
Guilherme Freitas <95315019+guiFreitasDev1@users.noreply.github.com>
2022-06-11 18:52:44 -0300

    1

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,9 @@
-function criarHoraDosSegundos(segundos){
-    const data = new Date (segundos * 1000)
+function criarHoraDosSegundos(segundos) {
+    const data = new Date(segundos * 1000)
     return data.toLocaleTimeString('pt-br', {
-    hour12: false,
-    timeZone: 'UTC'
-})
+        hour12: false,
+        timeZone: 'UTC'
+    })
 }
 
 const relogio = document.querySelector('.relogio')
@@ -12,29 +12,54 @@ const pausar = document.querySelector('.pausar')
 const zerar = document.querySelector('.zerar')
 let segundos = 0;
 let timer;
+let p = document.querySelector('.P')
 
-function iniciaRelogio(){
-     timer = setInterval(function(){
+function iniciaRelogio() {
+    timer = setInterval(function () {
         segundos++;
         relogio.innerHTML = criarHoraDosSegundos(segundos)
     }, 1000)
 }
-iniciar.addEventListener('click', function(event){
-    clearInterval(timer)
-    iniciaRelogio()
-    relogio.classList.remove('pausado')
-    
-})
-pausar.addEventListener('click', function(event){
-   clearInterval(timer) 
-   relogio.classList.add('pausado')
-    
-})
-zerar.addEventListener('click', function(event){
-     clearInterval(timer)
-     relogio.innerHTML = '00:00:00'
-     segundos = 0;
-     relogio.classList.remove('pausado')
-   
 
+document.addEventListener('click', function (e) {
+    const el = e.target;
+
+    if (el.classList.contains('iniciar')) {
+        p.style.background= 'lightblue'
+        p.style.borderRadius= '5px'
+        p.style.color= 'black'
+        p.style.padding = '10px'
+        p.style.fontWeight = 'bold'  
+        clearInterval(timer)
+        iniciaRelogio()
+        relogio.classList.remove('pausado')
+        p.innerHTML = 'Você Iniciou o timer'
+    }
+    if (el.classList.contains('pausar')) {
+        p.style.background= 'red'
+        p.style.borderRadius= '5px'
+        p.style.color= 'white'
+        p.style.padding = '10px'
+        p.style.fontWeight = 'bold'
+        clearInterval(timer)
+        relogio.classList.add('pausado')
+        p.innerHTML = 'Você pausou o timer'
+
+    }
+    if (el.classList.contains('zerar')) {
+        p.style.background= 'black'
+        p.style.borderRadius= '5px'
+        p.style.color= 'white'
+        p.style.padding = '10px'
+        p.style.fontWeight = 'bold'
+        clearInterval(timer)
+        relogio.innerHTML = '00:00:00'
+        segundos = 0;
+        relogio.classList.remove('pausado')
+        p.innerHTML = 'Você zerou o timer'
+        
+
+    }
 })
+
+
