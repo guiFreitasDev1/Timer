534a8fd7d06af0c8debad738d9bf982bbf551957
Guilherme Freitas <95315019+guiFreitasDev1@users.noreply.github.com>
2022-06-11 19:56:56 -0300

    1.2

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,9 +8,6 @@ function criarHoraDosSegundos(segundos) {
 }
 
 const relogio = document.querySelector('.relogio')
-const iniciar = document.querySelector('.iniciar')
-const pausar = document.querySelector('.pausar')
-const zerar = document.querySelector('.zerar')
 let segundos = 0;
 let timer;
 let p = document.querySelector('.P')
